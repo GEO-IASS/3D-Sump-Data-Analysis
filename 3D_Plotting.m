9ddffdd560f4577c86edaf770337612eee3b4c9f
hendemic <services@michaelrhenderson.com>
2016-03-02 14:49:34 -0800

    Initial checkin

---
+++ 3D_Plotting.m
@@ -0,0 +1,40 @@
+%1. Create new variable and rename to raw_data
+%2. Copy x,y,z point cloud data into variable contents
+%3. Run script
+
+%clear window and all variables except for raw_data
+clc
+clearvars -except raw_data
+
+
+n = 5;  %resolution of meshgrid
+
+%Extract X, Y, Z as arrays from raw_data matrix
+X = abs(raw_data(:,1));
+Y = abs(raw_data(:,2));
+Z = raw_data(:,3);
+
+%Define meshgrid ranges
+xRange = min(X)+n:n:max(X)-n;
+yRange = min(Y)+n:n:max(Y)-n;
+
+%Define meshgrid matrices
+[Xq,Yq] = meshgrid(xRange, yRange);
+
+%Interpolate Z values on meshgrid
+Zq = griddata(X,Y,Z,Xq,Yq);
+
+%Plot surface
+figure(1)
+set(gcf,'Renderer','painters')
+surf(Xq, Yq, Zq);
+title('Spread Deck Flatness')
+zlim([35 40])
+
+%Plot contour
+figure(2)
+set(gcf,'Renderer','painters')
+contourf(Xq, Yq, Zq, 50);
+title('Spread Deck Flatness')
+zlim([35 40])
+
